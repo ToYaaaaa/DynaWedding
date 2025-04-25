08b674190d371666d15889f12699ec68dfa88bb9
Fariz abdulfatah <farizabdufatah245@gmail.com>
2025-04-25 10:41:46 +0700

    menambahkan bagian reservasi

--- js/script.js
+++ js/script.js
@@ -65,3 +65,25 @@ function enableScroll(){
 if(!localStorage.getItem('opened')){
     disableScroll()
 }
+
+// mematikan redirect pada form kehadiran
+
+window.addEventListener("load", function() {
+    const form = document.getElementById('my-form');
+    form.addEventListener("submit", function(e) {
+      e.preventDefault();
+      const data = new FormData(form);
+      const action = e.target.action;
+      fetch(action, {
+        method: 'POST',
+        body: data,
+      })
+      .then(() => {
+        Swal.fire({
+            title: "Berhasil",
+            text: "Terimakasih Telah Konfirmasi",
+            icon: "success"
+          });
+      })
+    });
+  });
